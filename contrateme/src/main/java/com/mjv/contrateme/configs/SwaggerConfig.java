8733f45d8b387befaded438228705cf0e545841f
Jonathan Zapotosczny <jonathangm.hd@gmail.com>
2023-07-03 11:25:57 -0300

    iniciando o projeto com configurações

---
+++ contrateme/src/main/java/com/mjv/contrateme/configs/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.mjv.contrateme.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(info()
+                .contact(contact()));
+    }
+
+    private Info info() {
+        return new Info()
+                .title("Contrate-me")
+                .description("API de Armazenamento de Dados Pessoais e Profissionais")
+                .version("1.0.0");
+    }
+
+    private Contact contact() {
+        return new Contact()
+                .name("Contrate-me")
+                .email("contato@contrateme.com")
+                .url("https://github.com/JonathanZapotosczny/mjv-java-school-grupo09-projeto-final");
+    }
+}
---
+++ contrateme/src/test/java/com/mjv/contrateme/ContratemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjv.contrateme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContratemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
